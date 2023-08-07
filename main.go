ddc8f90536543aad3817c2453d02f6d54e7eb8ed
yura7gi14 <yui.shiopi@gmail.com>
2023-08-07 23:04:15 +0900

    add:URLをAIに送信

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,7 +22,7 @@ func main() {
 	// Ginのルーターを初期化
 	r := gin.Default()
 
-	//
+	//URLを受け取る
 	r.POST("/process-url", func(c *gin.Context) {
 		var inputData struct {
 			URL string `form:"url" binding:"required"`
@@ -31,14 +34,60 @@ func main() {
 		}
 
 		// inputData.URL にフォームから送信されたURLが格納されているので、ここで適切な処理を行う
+		// 受け取ったURLをAIに送信して結果を取得
+		aiResponse, err := sendURLtoAI(inputData.URL)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error sending URL to AI"})
+			return
+		}
 
-		c.JSON(http.StatusOK, gin.H{"message": "URL received successfully"})
+		// AIからのレスポンス（0か1）をフロントに返す
+		c.JSON(http.StatusOK, aiResponse)
 	})
-
-	//受け取ったurlをAiに渡す
-
-	//AIから帰ってきた審議判定をフロントに返す
 
 	// サーバーを起動
 	r.Run(":8080")
+}
+
+// AIにURLを送信して結果を取得する関数
+// できててほしい
+func sendURLtoAI(url string) (int, error) {
+	data := map[string]string{"url": url}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return 0, err
+	}
+
+	aiURL := "http://localhost:5000/predict"
+
+	req, err := http.NewRequest("POST", aiURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return 0, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	var responseData map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&responseData)
+	if err != nil {
+		return 0, err
+	}
+
+	// responseDataから審議判定を取得して整数値に変換する
+	// この例では、"result" フィールドが 0 または 1 として返されることを仮定しています
+	result, ok := responseData["result"].(float64)
+	if !ok {
+		return 0, fmt.Errorf("Invalid response format")
+	}
+
+	return int(result), nil
 }
